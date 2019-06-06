37ef111e0548dcab259b70d8e448cbb419f1cc9b
Brittany Chiang <brittany.chiang@gmail.com>
2019-06-05 22:45:59 -0400

    Update header colors

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-'use strict';
-
 const WORKBENCH = '#171c28';
 const BACKGROUND = '#1d2433';
 const FOREGROUND = '#a2aabc';
@@ -9,10 +7,10 @@ const ACCENT = '#ffcc66';
 const RED = '#ef6b73';
 const GREEN = '#bae67e';
 const YELLOW = '#ffd580';
-const ORANGE = '#ffae57';
+// const ORANGE = '#ffae57';
 const BLUE = '#5ccfe6';
-const MAGENTA = '#c3a6ff';
 const PURPLE = '#c3a6ff';
+const MAGENTA = PURPLE;
 const WHITE = '#d7dce2';
 const LIGHT_GREY = '#8695b7';
 const GREY = '#2f3b54';
@@ -36,40 +34,43 @@ const colors = {
   lightCyan: BLUE,
   lightWhite: LIGHT_GREY,
   colorCubes: WHITE,
-  grayscale: UI_GREY
+  grayscale: UI_GREY,
 };
 
-exports.decorateConfig = config => {
-  return Object.assign({}, config, {
-    fontSize: 13,
-    fontFamily: 'Fira Mono for Powerline, Fira Mono, Fira Code, SF Mono, Menlo, Lucida Console, monospace',
-    fontWeightBold: 'normal',
-    cursorColor: ACCENT,
-    cursorShape: 'BEAM',
-    cursorBlink: true,
-    foregroundColor: FOREGROUND,
-    backgroundColor: BACKGROUND,
-    selectionColor: 'rgba(255, 204, 102, 0.5)',
-    borderColor: GREY,
-    css: `
-    ${config.css || ''}
+exports.decorateConfig = config => Object.assign({}, config, {
+  fontSize: 13,
+  fontFamily: 'Fira Mono for Powerline, Fira Mono, Fira Code, SF Mono, Menlo, Lucida Console, monospace',
+  fontWeight: 'normal',
+  fontWeightBold: 'normal',
+  lineHeight: 1.3,
+  cursorColor: ACCENT,
+  cursorShape: 'BEAM',
+  cursorBlink: true,
+  foregroundColor: FOREGROUND,
+  backgroundColor: BACKGROUND,
+  selectionColor: GREY,
+  borderColor: GREY,
+  css: `
     .hyper_main {
       border: none !important;
     }
+    .header_header {
+      background: ${WORKBENCH} !important;
+    }
     .tab_tab {
       color: ${FOREGROUND};
-      background: ${BACKGROUND};
+      background: ${WORKBENCH} !important;
+      border: 0;
     }
-    .tab_active {
-      border-bottom: 1px solid ${ACCENT} !important;
-      border-color: ${ACCENT};
+    .tab_tab.tab_active {
       color: ${WHITE};
-      background: rgba(47, 59, 84, 0.3);
+      border-bottom: 1px solid ${ACCENT} !important;
+      background: ${BACKGROUND} !important;
     }
-    x-row {
-      font-size: 20px !important;
+    .tabs_borderShim {
+      border-color: transparent !important;
     }
+    ${config.css || ''}
     `,
-    colors,
-  });
-};
+  colors,
+});
